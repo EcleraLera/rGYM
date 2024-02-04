0da40e6b7373aaeb55756852f4e2bcc0fc396e65
EcleraLera <borisowa.valerija2015@yandex.ru>
2024-02-04 14:24:35 +0300

    scroll first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const parent = document.querySelector('.container');
+
+const children = parent.children;
+
+const blockNames = document.getElementsByClassName("header hero");
+
+console.log(blockNames);
