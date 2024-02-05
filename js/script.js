602cdb80cf86bdb1c452acfde3044ede464bdc01
EcleraLera <borisowa.valerija2015@yandex.ru>
2024-02-05 14:38:31 +0300

    scroll second commmit

--- js/script.js
+++ js/script.js
@@ -2,6 +2,25 @@ const parent = document.querySelector('.container');
 
 const children = parent.children;
 
-const blockNames = document.getElementsByClassName("header hero");
+const childrenClass = children.classList;
 
-console.log(blockNames);
+console.log(childrenClass);
+
+console.log(window.innerHeight);
+
+
+const next = document.querySelector('.next');
+console.log(next);
+next.addEventListener("click", nextSectionScroll);
+
+function nextSectionScroll() {
+
+}
+
+const prev = document.querySelector('.prev');
+console.log(prev);
+prev.addEventListener("click", previousSectionScroll);
+
+function previousSectionScroll() {
+
+}
