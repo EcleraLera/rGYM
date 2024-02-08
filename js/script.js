27576c74285ff64d3afed24bafc6aa167b614ffa
EcleraLera <borisowa.valerija2015@yandex.ru>
2024-02-08 18:06:37 +0300

    scroll prev commit

--- js/script.js
+++ js/script.js
@@ -1,26 +1,46 @@
-const parent = document.querySelector('.container');
-
-const children = parent.children;
-
-const childrenClass = children.classList;
-
-console.log(childrenClass);
-
-console.log(window.innerHeight);
+const header = document.querySelector('.header');
+const hero = document.querySelector('.hero');
+const popular = document.querySelector('.popular');
+const individual = document.querySelector('.individual');
+const gallery = document.querySelector('.gallery');
+const feedback = document.querySelector('.feedback');
+const footer = document.querySelector('.footer');
 
 
-const next = document.querySelector('.next');
-console.log(next);
-next.addEventListener("click", nextSectionScroll);
+// // можно так записать в переменную значение высоты и топ
+// const headerTop = header.getBoundingClientRect().top;
+// const heroTop = hero.getBoundingClientRect().top;
+// const popularTop = popular.getBoundingClientRect().top;
+// const individualTop = individual.getBoundingClientRect().top;
+// const galleryTop = gallery.getBoundingClientRect().top;
+// const feedbackTop = feedback.getBoundingClientRect().top;
+// const footerTop = footer.getBoundingClientRect().top;
+// // console.log(heroCoord);
+// const next = document.querySelector('.next');
+// // console.log(next);
+// next.addEventListener("click", nextSectionScroll);
 
-function nextSectionScroll() {
+// function nextSectionScroll() {
 
-}
+// }
 
 const prev = document.querySelector('.prev');
-console.log(prev);
+
 prev.addEventListener("click", previousSectionScroll);
 
 function previousSectionScroll() {
-
+    if (window.pageYOffset > 0 && window.pageYOffset <= 80) {
+        header.scrollIntoView();
+    } else if (window.pageYOffset > 80 && window.pageYOffset <= 930) {
+        hero.scrollIntoView();
+    } else if (window.pageYOffset > 930 && window.pageYOffset <= 2037) {
+        popular.scrollIntoView();
+    } else if (window.pageYOffset > 2037 && window.pageYOffset <= 3165) {
+        individual.scrollIntoView();
+    } else if (window.pageYOffset > 3165 && window.pageYOffset <= 2037) {
+        gallery.scrollIntoView();
+    } else if (window.pageYOffset > 930 && window.pageYOffset <= 2037) {
+        feedback.scrollIntoView();
+    } 
 }
+
